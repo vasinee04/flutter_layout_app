d0402102029a8322dae5c7a1c2e3cacbfe865935
vasinee04 <mr.unging2546@gmail.com>
2025-06-27 13:33:07 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Flutter App"),
+          backgroundColor: const Color.fromARGB(255, 249, 128, 77),
+        ),
+        body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [Image.asset("assets/images/ui1.png")],
+        ),
+      ),
+    );
+  }
+}
